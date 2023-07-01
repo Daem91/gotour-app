146d8c386672428d4aca9bece9e4e99aff5962d9
Carlos Lizarzaburu <carlos_abanto2@hotmail.com>
2023-07-01 13:26:21 -0500

    Sección Búsqueda - Búsqueda por Tags

--- app/src/main/java/com/dapm/gotour/home/SearchActivity.kt
+++ app/src/main/java/com/dapm/gotour/home/SearchActivity.kt
@@ -79,7 +79,7 @@ class SearchActivity : AppCompatActivity() {
         }
 
         val tags = listOf("Histórico","Cultural","Arquitectura","Patrimonio","Museo","Punto de encuentro","Naturaleza","Aventura","Paisajes","Trekking","Monumento","Construcción tradicional","Ecoturismo","Vida silvestre","Selva amazónica","Recreación","Parque temático","Animales","Arte","Vista al mar","Paseo costero","Actividades al aire libre","Restaurantes y tiendas","Playa","Surf","Relax","Atardeceres")
-//        generateTagViews(tags)
+        generateTagViews(tags)
 
         recyclerView = findViewById(R.id.destinos_recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(this)
@@ -91,13 +91,16 @@ class SearchActivity : AppCompatActivity() {
         destinoAdapter = DestinoAdapter(destinos)
         recyclerView.adapter = destinoAdapter
 
+        val ciudadTextView = TextView(this)
+        ciudadTextView.text = ""
+
         binding.barraBusqueda.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 return false
             }
 
             override fun onQueryTextChange(query: String?): Boolean {
-                filtrarDestinos(query)
+                filtrarDestinos(query, getSelectedTags())
                 return true
             }
 
@@ -105,28 +108,91 @@ class SearchActivity : AppCompatActivity() {
 
     }
 
-    fun filtrarDestinos(query: String?) {
+    fun filtrarDestinos(query: String?, selectedTags: List<String>) {
 
         val dataBaseHandler = DataBaseHandler(this)
-
         val destinos = dataBaseHandler.obtenerTodosLosDestinos()
+        val listaFiltrada = ArrayList<Destino>()
+
+        println(selectedTags)
 
         if (query != null) {
-            val listaFiltrada = ArrayList<Destino>()
-            for (i in destinos) {
-                if (i.nombre.lowercase(Locale.ROOT).contains(query.lowercase(Locale.ROOT))){
-                    listaFiltrada.add(i)
+            for (destino in destinos) {
+                if (destino.nombre.lowercase(Locale.ROOT).contains(query.lowercase(Locale.ROOT))){
+                    listaFiltrada.add(destino)
                 }
-            }
-
-            if (listaFiltrada.isEmpty()) {
-                binding.resultadosVisibilidad.visibility = View.VISIBLE
-                destinoAdapter.setListaFiltrada(listaFiltrada)
-            } else {
-                binding.resultadosVisibilidad.visibility = View.GONE
-                destinoAdapter.setListaFiltrada(listaFiltrada)
             }
         }
 
+        if (selectedTags.isNotEmpty()) {
+            val listaFiltradaPorTags = ArrayList<Destino>()
+            for (destino in listaFiltrada) {
+                for (tag in selectedTags) {
+                    if (destino.tags.any{it.equals(tag, ignoreCase = true)}) {
+                        listaFiltradaPorTags.add(destino)
+                        break
+                    }
+                }
+            }
+            listaFiltrada.clear()
+            listaFiltrada.addAll(listaFiltradaPorTags)
+        }
+
+        if (listaFiltrada.isEmpty()) {
+            binding.resultadosVisibilidad.visibility = View.VISIBLE
+        } else {
+            binding.resultadosVisibilidad.visibility = View.GONE
+        }
+        destinoAdapter.setListaFiltrada(listaFiltrada)
+    }
+    private fun generateTagViews(tags: List<String>){
+        val tagContainer: LinearLayout = findViewById(R.id.tags_search)
+        for (tag in tags) {
+            val tagTextView  = createTagView(tag)
+            tagContainer.addView(tagTextView)
+        }
+    }
+
+    private fun createTagView(tag: String): View {
+        val tagToggleButton  = ToggleButton(this)
+        tagToggleButton.text = tag
+        tagToggleButton.textOff = tag
+        tagToggleButton.textOn = tag
+        tagToggleButton.setBackgroundResource(R.drawable.tag_background)
+        val color = ContextCompat.getColor(this, R.color.orange)
+        tagToggleButton.setTextColor(color)
+        tagToggleButton.setSingleLine(true)
+        tagToggleButton.setPadding(30,0,30,0)
+
+        val layout = FlexboxLayout.LayoutParams(
+            FlexboxLayout.LayoutParams.WRAP_CONTENT,
+            FlexboxLayout.LayoutParams.WRAP_CONTENT
+        )
+        layout.setMargins(8, 0, 8, 8)
+        tagToggleButton.layoutParams = layout
+
+        tagToggleButton.setOnCheckedChangeListener{ _, isChecked ->
+            if (isChecked){
+                tagToggleButton.setTextColor(resources.getColor(R.color.white))
+                tagToggleButton.setBackgroundResource(R.drawable.tag_background_orange)
+            } else {
+                tagToggleButton.setTextColor(resources.getColor(R.color.orange))
+                tagToggleButton.setBackgroundResource(R.drawable.tag_background)
+            }
+            filtrarDestinos(binding.barraBusqueda.query.toString(), getSelectedTags())
+        }
+
+        return tagToggleButton
+    }
+
+    private fun getSelectedTags(): List<String> {
+        val selectedTags = ArrayList<String>()
+        for (i in 0 until binding.tagsSearch.childCount) {
+            val childView = binding.tagsSearch.getChildAt(i)
+            if (childView is ToggleButton && childView.isChecked) {
+                selectedTags.add(childView.text.toString())
+            }
+        }
+        return selectedTags
     }
 }
